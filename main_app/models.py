578e7e7620c09fd1b8b2d683d05be895aef160b5
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-03-11 14:21:37 -0700

    add Model & detail view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Bachelor(models.Model): 
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    wealth_origin = models.TextField(max_length = 250)
+    location = models.CharField(max_length = 100)
+    net_worth = models.IntegerField()
+    dealbreaker = models.TextField(max_length = 250)
+
+    def __str__(self):
+        return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),    
     path('bachelors/', views.bachelors_index, name = "index"),
+    path('bachelors/<int:bachelor_id>/', views.bachelors_detail, name = "detail")
 ]
