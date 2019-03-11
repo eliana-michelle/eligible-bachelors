1d3d74cfc5cccc3969ec1281b844d6cc22472bcb
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-03-11 14:59:33 -0700

    update formatting

--- main_app/views.py
+++ main_app/views.py
@@ -10,7 +10,7 @@ def about(request):
     return render(request, 'about.html')
 
 def bachelors_index(request):
-    bachelors = Bachelor.objects.all()
+    bachelors = Bachelor.objects.all().order_by('-net_worth')
     return render(request, 'bachelors/index.html', {'bachelors': bachelors})
 
 def bachelors_detail(request, bachelor_id):
